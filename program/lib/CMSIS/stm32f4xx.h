6a68010dd16ac19141e3cae2604e3417bde28bba
zxc2694 <zxc2694zxc2694@gmail.com>
2015-07-19 15:57:22 +0800

    Add configuration of STM32F429 in stm32f4xx.h, and choose it.

--- program/lib/CMSIS/stm32f4xx.h
+++ program/lib/CMSIS/stm32f4xx.h
@@ -92,6 +92,10 @@
   #define STM32F427_437xx
 #endif /* STM32F427X */
 
+#ifdef STM32F429
+  #define STM32F429_439xx
+#endif /* STM32F429 */
+
 /*  Tip: To avoid modifying this file each time you need to switch between these
         devices, you can define the device in your toolchain compiler preprocessor.
   */
