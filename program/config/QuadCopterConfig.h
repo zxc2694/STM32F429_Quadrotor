52eb196909b239367bbda7b6dc2b56d33926b60f
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2015-07-21 19:30:05 +0800

    Arrange and align the code in QuadCopterConfig.h

--- program/config/QuadCopterConfig.h
+++ program/config/QuadCopterConfig.h
@@ -2,31 +2,36 @@
 #define __QUADCOPTER_CONFIG_H
 
 /* Flight Control Board */
-#define Use_QCopter_board	0
+#define Use_QCopter_board   0
 #define Use_stm32f407_board 0
 #define Use_stm32f429_board 1
+
 /*Radio control*/
-#define USE_FUTABA_CONTROLLER		0
-#define USE_WFLY_CONTROLLER		1
+#define USE_FUTABA_CONTROLLER   0
+#define USE_WFLY_CONTROLLER     1
 #define USE_DEVO_v10_CONTROLLER 0
+
 /*IMU Sensor*/
-#define USE_MPU9150_and_MS5611 1
-#define USE_GY86 0
+#define USE_MPU9150_and_MS5611  1
+#define USE_GY86                0
+
 /* Hardware environments */
-#define configRemoteControl			0       //if you didn't connect motor -> set 0. 
-#define configSensor				1       //if you didn't connect sensor -> set 0. 
-#define configSD					0       //if you didn't connect SD card -> set 0.
-#define configNRF					0       //if you didn't connect nRF module -> set 0.
-#define configULTRANSONIC			0       //if you didn't connect ultransonic -> set 0. 
+#define configRemoteControl     1       //if you didn't connect remote control -> set 0. 
+#define configSensor            1       //if you didn't connect sensor -> set 0. 
+#define configSD                0       //if you didn't connect SD card -> set 0.
+#define configNRF               0       //if you didn't connect nRF module -> set 0.
+#define configULTRANSONIC       0       //if you didn't connect ultransonic -> set 0. 
+
 /* Status report functions */
-#define configSTATUS_SHELL		1
-#define configSTATUS_GET_MOTORS	0
-#define configSTATUS_GET_ROLL_PITCH_YAW	0
+#define configSTATUS_SHELL                 1
+#define configSTATUS_GET_MOTORS            0
+#define configSTATUS_GET_ROLL_PITCH_YAW    0
 #define configSTATUS_GET_ROLL_PITCH_MOTORS 0
-#define configSTATUS_GET_ULTRASONIC 0
+#define configSTATUS_GET_ULTRASONIC        0
+
 /* gui3 */
-#define configSTATUS_GET_MAG 0
-#define configSTATUS_GET_ACC 0
+#define configSTATUS_GET_MAG  0
+#define configSTATUS_GET_ACC  0
 #define configSTATUS_GET_GYRO 0
 
 /* Set baudrate */
